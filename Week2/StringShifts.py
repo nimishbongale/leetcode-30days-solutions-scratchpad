f3f42875b9148e3b97815da511e32d8ba8fa6989
Nimish Bongale <nimishnb98@gmail.com>
2020-04-14 19:17:17 +0530

    Create StringShifts.py

---
+++ Week2/StringShifts.py
@@ -0,0 +1,12 @@
+class Solution:
+    def doshift(self,s,d,a):
+        if d==0:
+            return s[a:]+s[:a]
+        return s[len(s)-a:]+s[:len(s)-a]
+    
+    def stringShift(self, s: str, shift: List[List[int]]) -> str:
+        for i in shift:
+            s=self.doshift(s,i[0],i[1])
+            print(s)
+        return s
+        
