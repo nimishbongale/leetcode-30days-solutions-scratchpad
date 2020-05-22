7a8073b474df251a36038dedb6c17d9d98a9cd3c
Nimish Bongale <nimishnb98@gmail.com>
2020-05-22 13:46:13 +0530

    Create SortCharBasedOnFrequency.py

---
+++ April/Week4/SortCharBasedOnFrequency.py
@@ -0,0 +1,9 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        d=Counter(s)
+        ans=''
+        k=sorted(d,key=lambda x:d[x],reverse=True)
+        for i in k:
+            ans+=i*d[i]
+        return ans
+        
