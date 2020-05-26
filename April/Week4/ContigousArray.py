f058158d782475cc0d0e3d575fbc3a8681fe9b80
Nimish Bongale <nimishnb98@gmail.com>
2020-05-26 19:11:07 +0530

    Create ContigousArray.py

---
+++ April/Week4/ContigousArray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        stor,sum1,max1={},0,0
+        for i in range(len(nums)):
+            sum1+=1 if nums[i]==1 else -1
+            if sum1==0:
+                max1=i+1
+            elif sum1 not in stor.keys():
+                stor[sum1]=i
+            else:
+                max1=max(i-stor[sum1],max1)
+        return max1
+                
+            
+        
