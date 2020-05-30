cda63254601b03c760d5513f35e53525d2a55b91
Nimish Bongale <nimishnb98@gmail.com>
2020-05-30 19:49:37 +0530

    Create KClosesPointsToOrigin.py

---
+++ April/Week5/KClosesPointsToOrigin.py
@@ -0,0 +1,8 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        dist={}
+        for i in points:
+            dist[tuple(i)]=sqrt(i[0]**2+i[1]**2)
+        k=sorted(dist,key=lambda x:dist[x])
+        return k[:K]
+        
